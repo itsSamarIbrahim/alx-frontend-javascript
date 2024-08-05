ec3115d8f7db1163220433c3ae0d63a7b07ce386
Samar Alx <bantmasr21@gmail.com>
2024-08-05 19:22:43 +0000

    8-getBudgetCurrentYear.js

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+  const budget = {
+
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
