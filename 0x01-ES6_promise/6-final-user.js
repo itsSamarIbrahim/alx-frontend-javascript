414dba0becd715a420763f2fd908f9b763a726f8
Samar Alx <bantmasr21@gmail.com>
2024-08-07 19:15:52 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,13 +6,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => {
-      return results.map((result) => {
-        if (result.status === 'fulfilled') {
-          return { status: 'filfilled', value: result.value };
-	} else {
-          return { status: 'rejected', value: result.reason };
-	}
-      });
-    });
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'filfilled', value: result.value };
+      }
+      return { status: 'rejected', value: String(result.reason) };
+    }));
 }
