becccced755d1134a4bec158157a7eef2848f9d9
Samar <bantmasr21@gmail.com>
2024-08-13 16:11:50 +0300

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdSum(students) {
-    return students.reduce((sum, student) => sum + student.id, 0);
+  return students.reduce((sum, student) => sum + student.id, 0);
 }
