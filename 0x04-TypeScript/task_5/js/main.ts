c99f7f852358f38fd52185e4f13ed5833c3a7db0
Samar <bantmasr21@gmail.com>
2024-08-15 02:33:50 +0300

    added

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -22,12 +22,12 @@ function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorC
     };
 }
 
-// Example usage (you can remove or comment out this part if you don't want to log anything):
+// Example usage:
 const major1: MajorCredits = { credits: 3, brand: 'Major' };
 const major2: MajorCredits = { credits: 4, brand: 'Major' };
 
 const minor1: MinorCredits = { credits: 2, brand: 'Minor' };
 const minor2: MinorCredits = { credits: 1, brand: 'Minor' };
 
-console.log(sumMajorCredits(major1, major2)); // { credits: 7, brand: 'Major' }
-console.log(sumMinorCredits(minor1, minor2)); // { credits: 3, brand: 'Minor' }
+console.log(sumMajorCredits(major1, major2));
+console.log(sumMinorCredits(minor1, minor2));
